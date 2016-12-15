69334c35c4280e34359e72738ac5e97075c5402e
campsx <cam.laurent@outlook.com>
2016-12-15 11:49:01 +0100

    Correction d'un bug d'init

--- functions.php
+++ functions.php
@@ -33,17 +33,3 @@ add_action('after_setup_theme', 'wpdocs_theme_setup');
 function wpdocs_theme_setup(){
   add_image_size( 'banner', 1240, 150, true);
 }
-
-/* custome post type */
-
-add_action('init' , 'custom_post_type');
-
-function custome_post_type() {
-  register_post_type('events', [
-    'labels' => [
-      'name' => __('events'),
-      'singular_name' => __('event')
-    ],
-    'public' => true
-  ]);
-}
