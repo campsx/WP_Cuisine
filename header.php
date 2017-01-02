593a32c758e39b1b0d62151156e3a2e9255df2c8
brixton <thorelrobin@yahoo.fr>
2017-01-02 16:44:07 +0100

    Ajout du font-awesome, finalisation du footer v1.0

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="http://127.0.0.1/WP_Cuisine/wp-content/themes/WP_Cuisine/style.css">
+    	<link rel="stylesheet" href="http://127.0.0.1/WP_Cuisine/wp-content/themes/WP_Cuisine/css/font-awesome.min.css">
     <title><?php
     wp_title();
     echo " | ";
