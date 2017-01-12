748bcc0215a24450b98491fd826c1fc6479746b4
campsx <cam.laurent@outlook.com>
2017-01-12 20:32:04 +0100

    Ajout de jquery, du walker pour le menu bootstrap et des modifications pour le header

--- functions.php
+++ functions.php
@@ -1,10 +1,12 @@
 <?php
 
-function wp_Cuisine_scripts(){
-
-  wp_enqueue_style( 'WP_Cuisine-style', get_stylesheet_uri() );
+add_action( 'after_setup_theme', 'wpt_setup' );
+if ( ! function_exists( 'wpt_setup' ) ) {
+    function wpt_setup() {
+       register_nav_menu( 'primary', __( 'Primary navigation', 'wptuts' ) );
+    }
 }
-add_action( 'wp_enqueue_scripts', 'wp_Cuisine_scripts' );
+require_once('WpBootstrapNavwalker.php');
 
 /* Add fonts to the Wordpress theme*/
 function wp_Cuisine_fonts(){
@@ -13,7 +15,7 @@ function wp_Cuisine_fonts(){
 add_action( 'wp_enqueue_scripts', 'wp_Cuisine_fonts' );
 
 function myMenus(){
-  register_nav_menu('main_menu', 'Menu principal');
+  //register_nav_menu('main_menu', 'Menu principal');
   register_nav_menu('footer_menu', 'Menu pied de page');
 }
 
@@ -28,11 +30,24 @@ function mySidebars(){
 }
 add_action('widgets_init', 'mySidebars');
 
+function add_jquery() {
+  wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-3.1.1.min');
+}
+add_action( 'wp_enqueue_scripts', 'add_jquery' );
 
-/* Add bootstrap to the Wordpress theme*/
-function theme_add_bootstrap() {
-	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
+/* Add bootstrap to the Wordpress theme */
+function script_add_bootstrap() {
 	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js');
 }
+add_action( 'wp_enqueue_scripts', 'script_add_bootstrap' );
 
-add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
+function style_add_bootstrap() {
+  wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
+}
+add_action('wp_enqueue_scripts', 'style_add_bootstrap');
+
+/* Add our style in the end for override */
+function wp_Cuisine_scripts(){
+  wp_enqueue_style( 'WP_Cuisine-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'wp_Cuisine_scripts' );
--- header.php
+++ header.php
@@ -12,9 +12,28 @@
   </head>
   <body <?php body_class(); ?>>
     <header>
-      <div class="row">
-        <div class="col-sm-1">Icone</div>
-        <div class="col-sm-3">Titre</div>
-        <div class="col-sm-8">NavBar</div>
-      </div>
+      <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand topnav" href="<?php bloginfo('url')?>"><?php bloginfo('name')?></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <?php
+                wp_nav_menu( array(
+                'menu' => 'top_menu',
+                'depth' => 2,
+                'container' => false,
+                'menu_class' => 'nav navbar-nav navbar-right',
+                'walker' => new WpBootstrapNavwalker())
+                );
+              ?>
+            </div>
+        </div>
+      </nav>
     </header>
