c8b3398e2ad855d455deab06f035206a44839fd4
J-Ramsey <romain.eggermont@yahoo.fr>
2017-02-02 11:41:33 +0100

    Better presentation of category

--- category.php
+++ category.php
@@ -4,31 +4,31 @@ get_header();
 
 <section class="accueil">
 
-    <div class="container">
-      <h1 class="title-page">Category : <?php wp_title(); ?></h1>
-
-        <div class="div_accueil">
-            <h2 class="title">Derniere recettes</h2>
-            <?php
-              if (have_posts()):
+  <div class="container">
+    <h2 class="title-page">Catégorie <?php wp_title(); ?></h2>
+      <div class="div_accueil">
+        <h2 class="title">Dernières recettes</h2>
+        <?php
+            if (have_posts()):
                 while (have_posts()):
-                  the_post();
-            ?>
-                  <div <?php post_class();?> id="post-<?php the_ID(); ?>">
-                    <h3><?php the_title();?></h3>
-                    <?php the_post_thumbnail('medium'); ?>
-                    <?php the_content(); ?>
-                    <a href="<?php the_permalink(); ?>">Voir</a>
-                  </div>
-            <?php
+                    the_post();
+        ?>
+                    <section class="" 
+                        <?php post_class();?> id="post-<?php the_ID(); ?>">
+                        <h3><a href="<?php the_permalink(); ?>">Recette : <?php the_title();?></a></h3>
+                        <?php the_post_thumbnail('medium'); ?>
+                        <p class><?php the_content(); ?>
+                        <p class="text-right">Par : <?php the_author(); ?> </p>
+                        <hr>
+                    </div>
+        <?php
                 endwhile;
-              else:
+            else:
                 echo "<h3>Pas d'article</h3>";
-              endif;
-             ?>
-        </div>
-
-    </div>
+            endif;
+        ?>
+      </div>
+  </div>
 
 </section>
 
