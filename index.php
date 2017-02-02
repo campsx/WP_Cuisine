4dba1500bf89c469041d97139f78971fb43f8d72
J-Ramsey <romain.eggermont@yahoo.fr>
2017-02-02 10:54:22 +0100

    Correct indent of index.php

--- index.php
+++ index.php
@@ -15,42 +15,34 @@ get_header();
 
     <div class="container">
 
-        <div class="div_accueil">
-            <h2 class="title">Derniere recettes</h2>
-            <?php
-              if (have_posts()):
-                while (have_posts()):
-                  the_post();
-            ?>
-                  <div <?php post_class();?> id="post-<?php the_ID(); ?>">
-                    <h3><?php the_title();?></h3>
-                    <?php the_post_thumbnail('medium'); ?>
-                    <?php the_content(); ?>
-                    <a href="<?php the_permalink(); ?>">Voir</a>
-                  </div>
-            <?php
-                endwhile;
-              else:
-                echo "<h3>Pas d'article</h3>";
-              endif;
-             ?>
-        </div>
-
-        <div class="div_accueil">
-            <h2 class="title">Recettes du moment</h2>
-        </div>
-        <div class="div_accueil">
-            <h2 class="title">Les indémodables</h2>
-        </div>
-
-    </div>
+      <div class="div_accueil">
+          <h2 class="title">Derniere recettes</h2>
+          <?php
+            if (have_posts()):
+              while (have_posts()):
+                the_post();
+          ?>
+                <div <?php post_class();?> id="post-<?php the_ID(); ?>">
+                  <h3><?php the_title();?></h3>
+                  <?php the_post_thumbnail('medium'); ?>
+                  <?php the_content(); ?>
+                  <a href="<?php the_permalink(); ?>">Voir</a>
+                </div>
+          <?php
+              endwhile;
+            else:
+              echo "<h3>Pas d'article</h3>";
+            endif;
+           ?>
+      </div>
+      <div class="div_accueil">
+          <h2 class="title">Recettes du moment</h2>
+      </div>
+      <div class="div_accueil">
+          <h2 class="title">Les indémodables</h2>
+      </div>
+  </div>
 
 </section>
 
-
-
-
-
-<?php
-get_footer();
-?>
+<?php get_footer();die();
