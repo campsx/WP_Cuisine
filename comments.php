5acca381bbf378e1fb04c4b2c6965dc6b3732313
Sinicha <sinirados@gmail.com>
2017-02-03 12:30:58 +0100

    Ajout de la saisie sur les commentaires

--- comments.php
+++ comments.php
@@ -13,6 +13,7 @@ if (!empty($post->post_password)) { // if there's a password
 
 ?>
 
+<?php /* Affichage des commentaires */?>
 <div class="comments">
 <?php if ($comments) : ?>
 	<h3>Commentaire</h3>
@@ -23,4 +24,31 @@ if (!empty($post->post_password)) { // if there's a password
 		<div class="comment_info"> Créé le <?php comment_date('j F, Y') ?> à <?php comment_time() ?></a></div>
 	<?php endforeach; ?>
 <?php endif; ?>
+</div>
+
+<?php /* Affichage de la saisie */ ?>
+<div class="comment_input">
+	<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+		<?php if ( $user_ID ) : ?>
+
+			<p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="D&eacute;connect&eacute; de ce compte">D&eacute;connection &raquo;</a></p>
+
+		<?php else : ?>
+
+			<p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="40" tabindex="1" />
+			<label for="author"><small>Nom <?php if ($req) echo "(requis)"; ?></small></label></p>
+
+			<p><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="40" tabindex="2" />
+			<label for="email"><small> E-mail <?php if ($req) echo "(requis)"; ?></small></label></p>
+
+		<?php endif; ?>
+
+		<p><textarea name="comment" id="comment" cols="60" rows="10" tabindex="4"></textarea></p>
+
+		<p><input name="submit" type="submit" id="submit" tabindex="5" value="Envoyer" />
+		<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
+		</p>
+
+		<?php do_action('comment_form', $post->ID); ?>
+	</form>
 </div>
