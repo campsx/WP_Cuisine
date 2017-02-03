f622bc70e2375376c72ea8423e6eeb943ac54221
campsx <cam.laurent@outlook.com>
2017-02-03 09:55:23 +0100

    Added search systeme

--- search.php
+++ search.php
@@ -1,1 +1,33 @@
-ici on trouve la page de recherche
+<?php
+  get_header();
+?>
+<section class="accueil">
+
+    <div class="container">
+
+      <div class="div_accueil col-md-7">
+          <h2 class="title">Recherche</h2>
+          <?php
+            if (have_posts()):
+              while (have_posts()):
+                the_post();
+          ?>
+                <div <?php post_class();?> id="post-<?php the_ID(); ?>">
+                  <h3><?php the_title();?></h3>
+                  <?php the_post_thumbnail('medium'); ?>
+                  <?php the_content(); ?>
+                  <a href="<?php the_permalink(); ?>">Voir</a>
+                </div>
+          <?php
+              endwhile;
+            else:
+              echo "<h3>Rien trouver</h3>";
+            endif;
+           ?>
+      </div>
+
+      <?php get_sidebar(); ?>
+  </div>
+</section>
+
+<?php get_footer(); ?>
