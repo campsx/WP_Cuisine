93df5abf93ca0726df8c6f1d3827caa39d0d3377
J-Ramsey <romain.eggermont@yahoo.fr>
2017-02-03 10:06:17 +0100

    Display change of single.php

--- single.php
+++ single.php
@@ -6,16 +6,27 @@ get_header();
 
     <div class="container">
         <h1 class="title-page">Recettes <?php wp_title(); ?></h1>
-        <section class="div_accueil" style="padding:10px;">
+        <section class="div_accueil col-md-7" style="padding:10px;">
             <?php
                 if (have_posts()):
                     while (have_posts()):
                         the_post();
             ?>          
-                        <article class="text-left" <?php the_ID();?> >
-                            <h2 class="title text-center"><?php the_title();?></h2>
-                            <p class="text-center"><?php the_category(",")?> proposé par <?php the_author(); ?> </p>
-                            <?php the_content(); ?>
+                        <article class="text-left two-colonnes">
+                            <div class="col-1">
+                                <?php if ( has_post_thumbnail() ) :
+                                    the_post_thumbnail();
+                                else:
+                                    echo '<img class="single-thumbnail" src="'. get_template_directory_uri() .'/img/no-image.jpg" alt="Pas d\'image pour cet article">';
+                                endif;
+                                ?>
+                            </div>
+                                <h2 class="title text-center"><?php the_title();?></h2>
+                                <p class="text-center"><?php the_category(",")?> proposé par <?php the_author(); ?> </p>
+                                <?php the_content(); ?>
+                            <div class="col-2">
+                                
+                            </div>
                         </article>
             <?php
                     endwhile;
