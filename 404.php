db0c403abecbc0deea2ac32c244732628d7ca527
brixton <thorelrobin@yahoo.fr>
2017-02-03 08:45:13 +0100

    Ajout de la page 404

--- 404.php
+++ 404.php
@@ -1,1 +1,20 @@
-Page not found
+<?php
+	
+	get_header();
+
+?>
+
+<section class="page404">
+<div class="container">
+    <div id="div404" style="">
+        <h1><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> 404</h1>
+        <p>La page demandée n'a pas été trouvé</p>
+    </div>
+</div>
+</section>
+
+<?php
+	
+	get_footer();
+
+?>
